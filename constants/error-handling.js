cf4984023dbbe3c040e82b6b0da561bc1875abf2
Andrew Tite <andrew.tite@weboniselab.com>
2023-09-08 15:02:54 -0300

    Update error-handling.js and index.js

---
+++ constants/error-handling.js
@@ -0,0 +1,20 @@
+module.exports = {
+    ERROR: {
+        INVALID_REQUEST: {
+            code: 400,
+            message: 'Invalid request',
+        },
+        UNAUTHORIZED: {
+            code: 401,
+            message: 'Unauthorized',
+        },
+        FORBIDDEN: {
+            code: 403,
+            message: 'Forbidden',
+        },
+        NOT_FOUND: {
+            code: 404,
+            message: 'Not found',
+        },
+    },
+};
--- index.js
+++ index.js
@@ -2,30 +2,25 @@ const Koa = require('koa');
 const dotenv = require('dotenv');
 dotenv.config();
 
+const constantErrors = require('./constants/error-handling');
+const { ERROR: _ERRORS } = require('./constants/error-handling');
+
 const app = new Koa();
 
 // add method to ctx
 app.context.date = Date();
 // add property to ctx
-app.context.userData = {
-  first: 'Andrew',
-  last: 'Tite',
-};
+// app.context.userData = {
+//   first: 'Andrew',
+//   last: 'Tite',
+// };
 
 app.use(ctx => {
-  // use state (like React)
-  ctx.state.user = 'Andrew Tite';
-  // request object usage
-  const from = ctx.request.origin || ctx.request.ip;
-  const method = ctx.request.method;
-  // use body (like Express)
-  // ctx.response.body = `Hello ${ctx.state.user}, it's ${ctx.date}!`;
-  // Output to console
-  console.log(from);
-  console.log(method);
-  // Send user data
-  // ctx.response.body = ctx.userData;
-  ctx.response.body = `Hello ${ctx.userData.first} ${ctx.userData.last}!`;
+  if (ctx.userData) {
+    return (ctx.body = `Hello ${ctx.userData.first} ${ctx.userData.last}`);
+  } else {
+    return ctx.throw(_ERRORS.UNAUTHORIZED.code, 'data required');
+  }
 });
 
 app.listen(process.env.PORT);
